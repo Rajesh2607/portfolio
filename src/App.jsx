6de48fc6d31aa600ec412b49ff8093552e3d8fc1
LINGALA RAJESH <rajeshraj26072004@gmail.com>
2025-02-24 20:00:05 +0530

    let go

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
+import IntroAnimation from "./components/IntroAnimation";
+import Homepage from "./components/Homepage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import HomePage from "./components/HomePage";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Certificates from "./components/Certificates";
-import IntroAnimation from "./components/IntroAnimation";
 
 const App = () => {
   const [showIntro, setShowIntro] = useState(true);
 
-  useEffect(() => {
-    // Check if user has visited before
-    const hasVisited = localStorage.getItem("hasVisited");
-    if (hasVisited) {
-      setShowIntro(false);
-    } else {
-      setTimeout(() => {
-        setShowIntro(false);
-        localStorage.setItem("hasVisited", "true");
-      }, 3000); // Adjust time for intro animation
-    }
-  }, []);
-
   return (
-    <Router>
+    <>
       {showIntro ? (
-        <IntroAnimation />
+        <IntroAnimation onFinish={() => setShowIntro(false)} />
       ) : (
         <div className="flex flex-col min-h-screen">
           <Navbar />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/certificates" element={<Certificates />} />
-            </Routes>
-          </main>
+          <br />
+          <Homepage />
+          <br />
           <Footer />
         </div>
       )}
-    </Router>
+    </>
   );
 };
 
